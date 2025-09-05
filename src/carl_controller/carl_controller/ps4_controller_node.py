2bad711fb04d4211f0576de96f1936148af475cf
Yakobi02 <100649854+Yakobi02@users.noreply.github.com>
2025-09-05 15:21:04 +0930

    Yep cool no doubt

--- src/carl_controller/carl_controller/ps4_controller_node.py
+++ src/carl_controller/carl_controller/ps4_controller_node.py
@@ -23,9 +23,6 @@ class ControllerCommandPublisher(Node):
 
         super().__init__('controller_command_publisher')
 
-        with open('config.yaml', 'r') as file:
-            self.config = yaml.safe_load(file)
-
         # ROS topics to publish from the controller inputs        
         self.controller_state_publisher_ = self.create_publisher(String, 'controller_state', 100)
 
