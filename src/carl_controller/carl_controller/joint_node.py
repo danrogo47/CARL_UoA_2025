bb609cfa2f5178583c54ba38ac977c48c8e5f623
Yakobi02 <100649854+Yakobi02@users.noreply.github.com>
2025-09-05 15:56:27 +0930

    b

--- src/carl_controller/carl_controller/joint_node.py
+++ src/carl_controller/carl_controller/joint_node.py
@@ -17,7 +17,7 @@ class JointNode(Node):
         with open('config_joint.yaml', 'r') as file:
             self.config = yaml.safe_load(file)
             
-        self.dynamixel = DynamixelController(self.config)
+        self.dynamixel = DynamixelController('config_joint.yaml')
             
         self.PIVOTS = self.config['motor_ids']['pivots']
         self.front_pivot_angle = self.config['pivot_parameters']['initial_front_angle']
